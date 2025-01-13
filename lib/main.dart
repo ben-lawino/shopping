7150cbc3553c01ac2b29bbf3d73e3099d8d8063f
Benjamin lawino <lawinwes@gmail.com>
2025-01-14 01:43:29 +0300

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:shopping/models/list_items.dart';
+import 'package:shopping/models/shopping_list.dart';
+import 'package:shopping/util/dbhelper.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,14 +14,48 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Shopping List',
       theme: ThemeData(
         primarySwatch: Colors.blue
       ),
-      home: Container(),
+      home: Scaffold(
+        appBar: AppBar(title: Text('Shopping List'),),
+        body: ShList(),
+      ),
     );
   }
 }
 
+class ShList extends StatefulWidget {
+  const ShList({super.key});
+
+  @override
+  State<ShList> createState() => _ShListState();
+}
+
+class _ShListState extends State<ShList> {
+  DbHelper helper = DbHelper();
+
+  Future showData () async{
+    await helper.openDb();
+    ShoppingList list = ShoppingList(0, 'Bakery', 2);
+    int listId = await helper.insertList(list);
+
+    ListItem item = ListItem(0, listId, 'Bread', '1kg', 'done');
+    int itemId = await helper.insertItem(item);
+
+    print('List Id: ' + listId.toString());
+    print('Item Id: ' + itemId.toString());
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    showData();
+    return Container();
+  }
+}
--- lib/util/dbhelper.dart
+++ lib/util/dbhelper.dart
@@ -1,35 +1,64 @@
 import 'package:flutter/material.dart';
 import 'package:path/path.dart';
+import 'package:shopping/models/list_items.dart';
+import 'package:shopping/models/shopping_list.dart';
 import 'package:sqflite/sqflite.dart';
 
 class DbHelper {
   final int version = 1;
-  Database? db; // Make the database nullable
+  Database? db;
 
+  // Open the database and enable foreign keys
   Future<Database> openDb() async {
     if (db == null) {
       db = await openDatabase(
         join(await getDatabasesPath(), 'shopping.db'),
         onCreate: (database, version) {
+          // Create the lists table with auto-incrementing id
           database.execute(
             'CREATE TABLE lists('
-                'id INTEGER PRIMARY KEY, '
+                'id INTEGER PRIMARY KEY AUTOINCREMENT, ' // Add AUTOINCREMENT for auto id
                 'name TEXT, '
                 'priority INTEGER)',
           );
+
+          // Create the items table with foreign key constraint
           database.execute(
             'CREATE TABLE items ('
-                'id INTEGER PRIMARY KEY, '
+                'id INTEGER PRIMARY KEY AUTOINCREMENT, ' // Add AUTOINCREMENT for auto id
                 'idList INTEGER, '
                 'name TEXT, '
                 'quantity TEXT, '
-                'notes TEXT, '
+                'note TEXT, '
                 'FOREIGN KEY(idList) REFERENCES lists(id))',
           );
         },
         version: version,
       );
+
+      // Enable foreign key support in SQLite
+      await db!.execute('PRAGMA foreign_keys = ON;');
     }
-    return db!; // Return the non-null database
+    return db!;
+  }
+
+  // Insert a shopping list into the database
+  Future<int> insertList(ShoppingList list) async {
+    // Insert the list into the 'lists' table and return the inserted id
+    int id = await this.db!.insert('lists',
+      list.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+    return id;
+  }
+
+  // Insert an item into the database
+  Future<int> insertItem(ListItem item) async {
+    // Insert the item into the 'items' table and return the inserted id
+    int id = await this.db!.insert('items',
+      item.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+    return id;
   }
 }
